eb1e4ef555efe7a50005779f63bd686b9e20ab3e
João Felipe P <jf.pelliccione@gmail.com>
2022-04-19 16:05:25 -0300

    Estruturando aplicação React - PT 1.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={ <Home /> } />
+      {/* <Route path="/register" element={ <Register /> } /> */}
+      {/* <Route path="/register/success" element={ <RegisterSuccess /> } /> */}
+      {/* <Route path="/login" element={ <Login /> } /> */}
+      {/* <Route path="/profile" element={ <Profile /> } /> */}
+      {/* <Route path="/profile/edit" element={ <EditProfileInfo /> } /> */}
+      {/* <Route path="/serverError" element={ <ServerError /> } /> */}
+      {/* <Route path="*" element={ <NotFound /> } /> */}
+    </Routes>
+  );
+}
+
+export default App;
