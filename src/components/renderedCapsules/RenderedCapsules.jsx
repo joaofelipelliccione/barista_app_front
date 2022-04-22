185c8c1f440a4e7eefe59900d2a17167abd60e30
João Felipe P <jf.pelliccione@gmail.com>
2022-04-22 15:53:53 -0300

    Montando estrutura.

--- src/components/renderedCapsules/RenderedCapsules.jsx
+++ src/components/renderedCapsules/RenderedCapsules.jsx
@@ -1,42 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import '../../styles/renderedCapsules/RenderedCapsules.css';
 
 function RenderedCapsules({ capsulesToRender }) {
   return (
     <section className="renderedCapsulesContainer">
       <h1>Capsulas</h1>
-      {console.log(capsulesToRender)}
+      <article className="capsulesCardsContainer">
+        {capsulesToRender.map((capsule) => (
+          <Link
+            to={ `/capsules/${capsule.capsuleId}` }
+            key={ capsule.capsuleId }
+            className="eachCapsuleCard"
+          >
+            <p>{capsule.capsuleId}</p>
+          </Link>
+        ))}
+      </article>
     </section>
   );
 }
 
 RenderedCapsules.propTypes = {
   capsulesToRender: PropTypes.arrayOf(PropTypes.shape({
-    capsule_id: PropTypes.number,
-    capsule_type: PropTypes.string,
-    capsule_line: PropTypes.string,
-    capsule_name: PropTypes.string,
+    capsuleId: PropTypes.number,
+    capsuleType: PropTypes.string,
+    capsuleLine: PropTypes.string,
+    capsuleName: PropTypes.string,
     decaf: PropTypes.bool,
-    capsule_intensity: PropTypes.number,
-    capsule_roasting_level: PropTypes.number,
-    capsule_bitterness_level: PropTypes.number,
-    capsule_acidity_level: PropTypes.number,
-    capsule_robustness_level: PropTypes.number,
-    capsule_aspects: PropTypes.string,
-    capsule_aromatic_notes: PropTypes.string,
-    capsule_origin: PropTypes.string,
+    capsuleIntensity: PropTypes.number,
+    capsuleRoastingLevel: PropTypes.number,
+    capsuleBitternessLevel: PropTypes.number,
+    capsuleAcidityLevel: PropTypes.number,
+    capsuleRobustnessLevel: PropTypes.number,
+    capsuleAspects: PropTypes.string,
+    capsuleAromaticNotes: PropTypes.string,
+    capsuleOrigin: PropTypes.string,
     ristretto25: PropTypes.bool,
     expresso40: PropTypes.bool,
-    dbl_expresso80: PropTypes.bool,
+    dblExpresso80: PropTypes.bool,
     lungo110: PropTypes.bool,
-    gran_lungo150: PropTypes.bool,
+    granLungo150: PropTypes.bool,
     coffe230: PropTypes.bool,
     carafe535: PropTypes.bool,
     cappuccino: PropTypes.bool,
-    dbl_cappuccino: PropTypes.bool,
-    capsule_img_src: PropTypes.string,
-    background_img_src: PropTypes.string,
+    dblCappuccino: PropTypes.bool,
+    capsuleImgSrc: PropTypes.string,
+    backgroundImgSrc: PropTypes.string,
   })).isRequired,
 };
 
