c050f02bc041707b53cda0919131b45d13bb86a2
João Felipe P <jf.pelliccione@gmail.com>
2022-04-23 18:25:59 -0300

    Construindo a página de detalhe da capsula.

--- src/pages/CapsuleDetails.jsx
+++ src/pages/CapsuleDetails.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 import Header from '../components/header/Header';
 import Footer from '../components/Footer';
 import '../styles/capsuleDetails/CapsuleDetails.css';
 
 function CapsuleDetails() {
+  const { id } = useParams();
+  const allCapsulesArr = useSelector((state) => state.capsules).allCapsules;
+  const capsuleInfo = allCapsulesArr.find(({ capsuleId }) => capsuleId === Number(id));
+
   return (
     <div className="capsuleDetailsPage">
+      {console.log(capsuleInfo)}
       <Header />
       <main className="capsuleDetailsMain">
-        Oi
+        <section className="capsuleDetailsContainer1">
+          OI
+        </section>
       </main>
       <Footer />
     </div>
