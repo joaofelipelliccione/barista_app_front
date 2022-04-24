ccf52bc9ad1000013a341cea4dc607e20d0d8f68
João Felipe P <jf.pelliccione@gmail.com>
2022-04-23 21:45:44 -0300

    Conclusão da lógica das cup sizes.

--- src/pages/CapsuleDetails.jsx
+++ src/pages/CapsuleDetails.jsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
-// import PropTypes from 'prop-types';
 import Header from '../components/header/Header';
 import Footer from '../components/Footer';
+
+import ristretto25 from '../images/cupSizes/img-ristretto25.svg';
+import expresso40 from '../images/cupSizes/img-expresso40.svg';
+import dblExpresso80 from '../images/cupSizes/img-dbl_expresso80.svg';
+import lungo110 from '../images/cupSizes/img-lungo110.svg';
+import granLungo150 from '../images/cupSizes/img-gran_lungo150.svg';
+import coffe230 from '../images/cupSizes/img-coffe230.svg';
+import carafe535 from '../images/cupSizes/img-carafe535.svg';
+import cappuccino from '../images/cupSizes/img-cappuccino.svg';
+import dblCappuccino from '../images/cupSizes/img-dbl_cappuccino.svg';
+
 import '../styles/capsuleDetails/CapsuleDetails.css';
 
 function CapsuleDetails() {
   const { id } = useParams();
   const allCapsulesArr = useSelector((state) => state.capsules).allCapsules;
   const capsuleInfo = allCapsulesArr.find(({ capsuleId }) => capsuleId === Number(id));
+  const cupSizesArray = [
+    { size: 'ristretto25', imgPath: ristretto25 },
+    { size: 'expresso40', imgPath: expresso40 },
+    { size: 'dblExpresso80', imgPath: dblExpresso80 },
+    { size: 'lungo110', imgPath: lungo110 },
+    { size: 'granLungo150', imgPath: granLungo150 },
+    { size: 'coffe230', imgPath: coffe230 },
+    { size: 'carafe535', imgPath: carafe535 },
+    { size: 'cappuccino', imgPath: cappuccino },
+    { size: 'dblCappuccino', imgPath: dblCappuccino },
+  ];
 
   return (
     <div className="capsuleDetailsPage">
@@ -29,16 +50,18 @@ function CapsuleDetails() {
           <h1>{ capsuleInfo.capsuleName }</h1>
         </div>
         <section className="capsuleDetailsContainer1">
-          OI
+          <div className="capsuleDetailsContainer1-1">
+            <h4>Medidas: </h4>
+            { cupSizesArray.map(({ size, imgPath }) => (
+              capsuleInfo[size]
+              && <img key={ size } src={ imgPath } alt="Cup size" className="cups" />
+            )) }
+          </div>
         </section>
       </main>
       <Footer />
     </div>
   );
 }
-
-// SearchMethods.propTypes = {
-//   searchMethod: PropTypes.string.isRequired,
-// };
 
 export default CapsuleDetails;
