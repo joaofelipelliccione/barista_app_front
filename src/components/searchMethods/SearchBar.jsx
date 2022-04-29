4cffc67db5775ada3f3c329db95c6a0882ec1f82
João Felipe P <jf.pelliccione@gmail.com>
2022-04-29 10:34:16 -0300

    Problema resolvido!

--- src/components/searchMethods/SearchBar.jsx
+++ src/components/searchMethods/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import Select from 'react-select';
 import { BsSearch } from 'react-icons/bs';
 import '../../styles/searchMethods/SearchBar.css';
@@ -10,12 +11,29 @@ function SearchBar({ capsulesArray, setIsFetching, setCapsulesToRender }) {
   const allCapsulesNamesArr = capsulesArray.map(({ capsuleName }) => capsuleName);
   const reactSelectArr = allCapsulesNamesArr.map((opt) => ({ label: opt, value: opt }));
 
-  const onClickSearchBtn = async () => {
-    const SEARCH_ONE_CAPSULE_ENDPOINT = `https://barista-app-back.herokuapp.com/capsules/search?capsuleName=${searchedCapsule}`;
+  const location = useLocation();
+  const currentPath = location.pathname;
 
+  const onClickSearchBtn = async () => {
+    const SEARCH_ONE_CAPSULE_FROM_ALL_ENDPOINT = `https://barista-app-back.herokuapp.com/capsules/search?capsuleType=All&capsuleName=${searchedCapsule}`;
+    const SEARCH_ONE_CAPSULE_FROM_ORIGINAL_ENDPOINT = `https://barista-app-back.herokuapp.com/capsules/search?capsuleType=Original&capsuleName=${searchedCapsule}`;
+    const SEARCH_ONE_CAPSULE_FROM_VERTUO_ENDPOINT = `https://barista-app-back.herokuapp.com/capsules/search?capsuleType=Vertuo&capsuleName=${searchedCapsule}`;
+
+    let data;
     setIsFetching(true);
-    const res = await fetch(SEARCH_ONE_CAPSULE_ENDPOINT);
-    const data = await res.json();
+
+    if (currentPath === '/') {
+      const res = await fetch(SEARCH_ONE_CAPSULE_FROM_ALL_ENDPOINT);
+      data = await res.json();
+    }
+    if (currentPath === '/capsules/original') {
+      const res = await fetch(SEARCH_ONE_CAPSULE_FROM_ORIGINAL_ENDPOINT);
+      data = await res.json();
+    }
+    if (currentPath === '/capsules/vertuo') {
+      const res = await fetch(SEARCH_ONE_CAPSULE_FROM_VERTUO_ENDPOINT);
+      data = await res.json();
+    }
 
     setCapsulesToRender(data);
     setIsFetching(false);
